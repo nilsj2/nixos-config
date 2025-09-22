547e1bc29f4db54b5b999e1f5df8b8258d2a02f1
Nils Juto <nilsjuto@posteo.net>
2025-09-22 22:50:05 +0200

    nvidia drivers

--- machines/workstation.nix
+++ machines/workstation.nix
@@ -11,6 +11,24 @@
   ];
   networking.hostName = "workstation";
 
+  nixpkgs.config.allowUnfree = true;
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false; # GTX 1060 is older than turing(GTX 1660)
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+
+    prime = {
+      sync.enable = true;
+      
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
   services.copyparty = {
     enable = true;
 
