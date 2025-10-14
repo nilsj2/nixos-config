e072b0a0d75bc625cacd75340ad69b74c2adb160
Nils Juto <nilsjuto@posteo.net>
2025-10-14 20:00:20 +0200

    regenerate hardware

--- machines/hardware/t480.nix
+++ machines/hardware/t480.nix
@@ -16,10 +16,8 @@
 
   boot.initrd.availableKernelModules = [
     "xhci_pci"
-    "ahci"
     "nvme"
-    "usbhid"
-    "uas"
+    "usb_storage"
     "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
@@ -49,8 +47,8 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
