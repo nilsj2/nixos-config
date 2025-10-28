c26385134b78773f3ae255e0abffeeaa082015eb
Nils Juto <nilsjuto@posteo.net>
2025-10-28 11:20:40 +0100

    update

--- flake.nix
+++ flake.nix
@@ -43,12 +43,14 @@
           ./machines/hardware/t480.nix
           ./machines/shared.nix
           ./users/nilsj/nixos.nix
-          { swapDevices =[
-            {
-              device = "/var/lib/swapfile";
-              size = 16 * 1024;
-            }
-          ];}
+          {
+            swapDevices = [
+              {
+                device = "/var/lib/swapfile";
+                size = 16 * 1024;
+              }
+            ];
+          }
         ]
         ++ homeManagerSetup;
       };
