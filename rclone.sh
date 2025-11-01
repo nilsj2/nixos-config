a3f0176ea331f3501a16b336740faadd74f665b7
Nils Juto <nilsjuto@posteo.net>
2025-11-01 15:23:52 +0100

    update

--- rclone.sh
+++ rclone.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone mount --vfs-cache-mode writes --dir-cache-time 5s 1761047289058-dav: ~/cpp --allow-non-empty
+rclone mount --vfs-cache-mode writes --dir-cache-time 5s nilssrv-dav: /home/nilsj/cpp
