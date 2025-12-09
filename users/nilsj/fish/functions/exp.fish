f2b624a67a546347e98958289dde487b1a19a993
Nils Juto <nilsjuto@posteo.net>
2025-12-09 21:53:52 +0100

    Some helper function for studying

---
+++ users/nilsj/fish/functions/exp.fish
@@ -0,0 +1,3 @@
+function exp --wraps='cd /home/nilsj/cpp/Documents/Plugg/Läkarprogrammet && lelon export > /tmp/cards.csv' --description 'alias exp=cd /home/nilsj/cpp/Documents/Plugg/Läkarprogrammet && lelon export > /tmp/cards.csv'
+    cd /home/nilsj/cpp/Documents/Plugg/Läkarprogrammet && lelon export > /tmp/cards.csv $argv
+end
---
+++ users/nilsj/fish/functions/transfer_imgs.fish
@@ -0,0 +1,3 @@
+function transfer_imgs --wraps='cp .assets/*.png /home/nilsj/.local/share/Anki2/Nils\\ Juto/collection.media/' --description 'alias transfer_imgs=cp .assets/*.png /home/nilsj/.local/share/Anki2/Nils\\ Juto/collection.media/'
+    cp .assets/*.png /home/nilsj/.local/share/Anki2/Nils\ Juto/collection.media/ $argv
+end
