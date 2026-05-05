2cf251947cc3ba871c5e961449b9419c02497346
Nils Juto <nilsjuto@posteo.net>
2026-05-05 20:12:10 +0200

    rerun nixos-generate-config

--- machines/hardware/thinkpad-t480.nix
+++ machines/hardware/thinkpad-t480.nix
@@ -42,14 +42,6 @@
     { device = "/dev/disk/by-uuid/7c29ae95-e735-45b7-8cb9-d76a50dcb447"; }
   ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
